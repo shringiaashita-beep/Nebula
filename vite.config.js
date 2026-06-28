17d57b755669877551f738a8774b5593f8f6da6a
shringiaashita-beep <shringiaashita@gmail.com>
2026-06-28 21:33:11 +0530

    vite.js update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/Nebula/",
-});
+  plugins: [react()],
+})
